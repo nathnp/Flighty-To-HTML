440a2de3082622348187b75394e9136aea46b8bd
Nathan <57642318+nathnp@users.noreply.github.com>
2025-03-13 15:35:33 -0400

    Flight taged as crew will not show airline

--- flights.sh
+++ flights.sh
@@ -2,36 +2,43 @@
 
 # Input CSV file
 INPUT_CSV="$1"
-OUTPUT_HTML="flights.html"
+OUTPUT_HTML="content/flights/flights.html"
 
+# Ensure an input file is provided
 if [[ -z "$INPUT_CSV" ]]; then
-	echo "Usage: $0 input.csv"
-	exit 1
+    echo "Usage: $0 input.csv"
+    exit 1
 fi
 
+# Extract the required columns and generate an HTML table
 {
-	echo "<html><body><table border='1'>"
-	echo "<tr><th>Date</th><th>Airline</th><th>Flight</th><th>Route</th><th>Aircraft Type Name</th><th>Tail Number</th></tr>"
-	
-	# Read CSV and extract required columns with airline name replacement, storing lines in an array
-	tail -n +2 "$INPUT_CSV" | awk -F',' 'BEGIN {
-		OFS="</td><td>";
-		airline["SWA"] = "Southwest";
-		airline["DAL"] = "Delta";
-		airline["NKS"] = "Spirit";
-	}
-	{
-		$2 = ($2 in airline) ? airline[$2] : $2;
-		route = $4 " ✈ " $5;
-		lines[NR] = "<tr><td>" $1 OFS $2 OFS $3 OFS route OFS $20 OFS $21 "</td></tr>";
-	}
-	END {
-		for (i = NR; i > 0; i--) {
-			print lines[i];
-		}
-	}'
-	
-	echo "</table></body></html>"
+    echo "<table border='1'>"
+    echo "<tr><th>Date</th><th>Airline</th><th>Flight</th><th>Route</th><th>Aircraft Type Name</th><th>Tail Number</th></tr>"
+    
+    # Read CSV and extract required columns with airline name replacement, storing lines in an array
+    tail -n +2 "$INPUT_CSV" | awk -F',' 'BEGIN {
+        OFS="</td><td>";
+        airline["SWA"] = "Southwest <img src='/assets/ali/swa.png' width='16'/>";
+        airline["DAL"] = "Delta <img src='/assets/ali/dal.png' width='16'/>";
+        airline["NKS"] = "Spirit <img src='/assets/ali/nks.png' width='16'/>";
+    }
+    {
+        if ($26 == "CREW") {
+            $2 = "Crew Flight";
+        } else {
+            $2 = ($2 in airline) ? airline[$2] : $2;
+        }
+        route = $4 " ✈ " $5;
+        lines[NR] = "<tr><td>" $1 OFS $2 OFS $3 OFS route OFS $20 OFS $21 "</td></tr>";
+    }
+    END {
+        for (i = NR; i > 0; i--) {
+            print lines[i];
+        }
+    }'
+    
+    echo "</table>"
 } > "$OUTPUT_HTML"
 
 echo "HTML table has been created in $OUTPUT_HTML"
+
